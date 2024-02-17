50502a3d52e31cce4579c3af63a22504df5719cd
emfriis <emilyfriis4@gmail.com>
2024-02-17 22:52:51 +1300

    Updated effect conditions to not trigger without targets where needed

--- monster-features/effect-only.js
+++ monster-features/effect-only.js
@@ -3,13 +3,13 @@
 //game.canvas.tokens.placeables.find(t=>t.actor&&!((t.actor?.system?.details?.type?.value==="custom"||t.actor?.system?.details?.type?.value==="")&&t.actor?.system?.details?.type?.custom==="")&&t.id!==canvas.tokens.controlled[0].id&&t.id!==game.user.targets?.first().id&&canvas.tokens.controlled[0].disposition===t.disposition&&t.actor?.system?.attributes?.hp?.value>0&&!(t.actor?.canvas.tokens.get(tokenId)?.actor?.items?.find(i=>i.id==workflow?.itemId)?.effects?.find(e=>["Incapacitated","Unconscious","Paralyzed","Petrified","Stunned"].includes(e.label)))&&MidiQOL.getDistance(t,game.user.targets?.first(),false)<=5)
 //game.canvas.tokens.placeables.find(t=>t.actor&&!((t.actor?.system?.details?.type?.value==="custom"||t.actor?.system?.details?.type?.value==="")&&t.actor?.system?.details?.type?.custom==="")&&t.id!==workflow.token.id&&t.id!==[...workflow.targets][0].id&&t.disposition===[...workflow.targets][0].disposition&&t.actor?.system?.attributes?.hp?.value>0&&!t.actor?.canvas.tokens.get(tokenId)?.actor?.items?.find(i=>i.id==workflow?.itemId)?.effects?.find(e=>["Incapacitated","Unconscious","Paralyzed","Petrified","Stunned"].includes(e.label))&&MidiQOL.getDistance(t,[...workflow.targets][0],false)<10)
 //game?.canvas?.tokens?.placeables?.find(t=>t.actor&&!((t.actor?.system?.details?.type?.value=="custom"||t.actor?.system?.details?.type?.value=="")&&t.actor?.system?.details?.type?.custom=="")&&t!=workflow.token&&t!=[...workflow.targets][0]&&t?.document?.disposition==workflow.token?.document?.disposition&&!MidiQOL.checkIncapacitated(t.actor)&&MidiQOL.computeDistance(t,[...workflow.targets][0],false)<10)
-game.canvas.tokens.placeables.find(t=>t.actor&&MidiQOL.typeOrRace(t.actor)&&t.id!=tokenId&&t.id!=targetId&&t?.document?.disposition==canvas.tokens.get(tokenId)?.document?.disposition&&!MidiQOL.checkIncapacitated(t.actor)&&MidiQOL.computeDistance(t,canvas.tokens.get(targetId),false)<10)
+workflow?.targets?.size&&canvas.tokens.placeables.find(t=>t.actor&&MidiQOL.typeOrRace(t.actor)&&t.id!=tokenId&&t.id!=targetId&&t?.document?.disposition==canvas.tokens.get(tokenId)?.document?.disposition&&!MidiQOL.checkIncapacitated(t.actor)&&MidiQOL.computeDistance(t,canvas.tokens.get(targetId),false)<10)
 
 //-------- blood frenzy - flags.midi-qol.advantage.attack.all
 //game.user.targets?.first()?.actor?.system?.attributes?.hp?.value < game.user.targets?.first()?.actor?.system?.attributes?.hp?.max
 //game.user.targets?.first()?.actor?.system?.attributes?.hp?.value<game.user.targets?.first()?.actor?.system?.attributes?.hp?.max
 //-------- [...workflow.targets][0].actor.system.attributes.hp.value<[...workflow.targets][0].actor.system.attributes.hp.max
-target.attributes.hp.value<target.attributes.hp.max
+workflow?.targets?.size&&target.attributes.hp.value<target.attributes.hp.max
 
 //--------- ambusher - flags.midi-qol.advantage.attack.all
 //game.combat?.round === 1 && game.user.targets?.first()?.actor?.effects?.find(e => e.label === "Surprised")
@@ -44,11 +44,11 @@ MidiQOL.computeDistance(workflow.token,[...workflow.targets][0],false)>Math.max(
 effects.find(e=>e.name=="Frightened"&&MidiQOL.canSee(canvas.tokens.get(tokenId),canvas.tokens.placeables.find(t=>t.actor&&(t.actor.id==e.origin.match(/Actor\.(.*?)\./)[1]))))
 
 //------mortal bulwark flags.midi-qol.advantage.attack.all
-["aberration","celestial","elemental","fey","fiend"].find(t=>MidiQOL.typeOrRace(targetActorUuid)?.toLowerCase().includes(t))
+workflow?.targets?.size&&["aberration","celestial","elemental","fey","fiend"].find(t=>MidiQOL.typeOrRace(targetActorUuid)?.toLowerCase().includes(t))
 
 //-----phalanx formation > flags.midi-qol.advantage.attack.all / flags.midi-qol.advantage.ability.save.dex
-workflow?.targets&&canvas.tokens.placeables.find(t=>t.actor&&MidiQOL.typeOrRace(t.actor)&&t?.document?.disposition==canvas.tokens.get(tokenId)?.document?.disposition&&t.id!=tokenId&&t.actor.items.find(i=>i.system?.armor?.type=="shield"&&i.system?.equipped)&&MidiQOL.computeDistance(t,canvas.tokens.get(tokenId),false)<10)
-workflow?.targets&&canvas.tokens.placeables.find(t=>t.actor&&MidiQOL.typeOrRace(t.actor)&&t?.document?.disposition==canvas.tokens.get(targetId)?.document?.disposition&&t.id!=targetId&&t.actor.items.find(i=>i.system?.armor?.type=="shield"&&i.system?.equipped)&&MidiQOL.computeDistance(t,canvas.tokens.get(targetId),false)<10)
+workflow?.targets?.size&&canvas.tokens.placeables.find(t=>t.actor&&MidiQOL.typeOrRace(t.actor)&&t?.document?.disposition==canvas.tokens.get(tokenId)?.document?.disposition&&t.id!=tokenId&&t.actor.items.find(i=>i.system?.armor?.type=="shield"&&i.system?.equipped)&&MidiQOL.computeDistance(t,canvas.tokens.get(tokenId),false)<10)
+workflow?.targets?.size&&canvas.tokens.placeables.find(t=>t.actor&&MidiQOL.typeOrRace(t.actor)&&t?.document?.disposition==canvas.tokens.get(targetId)?.document?.disposition&&t.id!=targetId&&t.actor.items.find(i=>i.system?.armor?.type=="shield"&&i.system?.equipped)&&MidiQOL.computeDistance(t,canvas.tokens.get(targetId),false)<10)
 
 //------underwater flags.midi-qol.disadvantage.attack.mwak/rwak flags.midi-qol.fail.attack.mwak/rwak
 !["dagger","javelin","shortsword","spear","trident"].includes(item?.baseItem)
@@ -93,6 +93,7 @@ canvas.tokens.get(tokenId)?.actor?.items?.find(i=>i.id==workflow?.itemId)?.effec
 canvas.tokens.get(tokenId)?.actor?.items?.find(i=>i.id==workflow?.itemId)?.effects?.find(e=>["turned"].find(s=>e.name.toLowerCase().includes(s))||e.changes?.find(c=>["turned"].find(s=>c.value.toLowerCase().includes(s))))||workflow?.targets&&canvas.tokens.get(targetId).actor?.effects?.find(e=>e.name==workflow?.item?.name&&e.changes?.find(c=>["turned"].find(s=>c.value.toLowerCase().includes(s))))
 //-----fey ancestry > flags.midi-qol.superSaver.all
 (workflow?.item?.type==spell||workflow?.item?.flags.midiProperties.magiceffect)&&canvas.tokens.get(tokenId)?.actor?.items?.find(i=>i.id==workflow?.itemId)?.effects?.find(e=>["unconscious"].find(s=>e.name.toLowerCase().includes(s))||e.changes?.find(c=>["unconscious"].find(s=>c.value.toLowerCase().includes(s))))||workflow?.targets&&canvas.tokens.get(targetId).actor?.effects?.find(e=>e.name==workflow?.item?.name&&e.changes?.find(c=>["unconscious"].find(s=>c.value.toLowerCase().includes(s))))
+//-----steadfast
 
 
 
